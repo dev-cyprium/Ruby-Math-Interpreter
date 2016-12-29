ace8fca9865777ba841b0e8bdbb8155ff576a4b6
Stefan Kupresak <stefan_vg@hotmail.com>
2016-12-29 12:37:36 +0100

    Add advance method

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -9,5 +9,22 @@ class Lexer
 		@current_char = text[0]
 	end
 
-	
+	def get_next_token
+
+	end
+
+	private
+	# Moves the pointer to the next character
+	def advance
+		@pos += 1
+		if @pos > @text.length - 1
+			@current_char = nil
+		else
+			@current_char = @text[@pos]
+		end
+	end
+
+	def error
+		raise "Unknown token exception"
+	end
 end
