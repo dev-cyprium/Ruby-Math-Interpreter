ef630b53f73ff8b436132c7b548442d01f7f0d76
Stefan Kupresak <stefan_vg@hotmail.com>
2016-12-29 14:09:20 +0100

    Add token specs

---
+++ spec/token_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../lib/token"
+
+describe Token do
+	describe 'Token.new' do
+		context 'Given Token::INTEGER, 5' do
+			it "Creates a new token with given values" do
+				t = Token.new(Token::INTEGER, 5)
+				expect(t.value).to eql(5)
+				expect(t.type).to eql(Token::INTEGER)
+			end
+		end
+
+		context 'Given Token::EPF, nil' do
+			it "Creates a new token with given values" do
+				t = Token.new(Token::EOF, nil)
+				expect(t.value).to eql(nil)
+				expect(t.type).to eql(Token::EOF)
+			end
+		end
+	end
+end
