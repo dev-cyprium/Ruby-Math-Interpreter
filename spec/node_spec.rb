1fa35335b7d17a4c48251c15af173d4ae8a61b4b
Stefan Kupresak <stefan_vg@hotmail.com>
2016-12-30 12:27:07 +0100

    Add BinOp tests

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,7 +4,6 @@ require_relative '../lib/node'
 describe AST do
 	describe 'Numeric node' do
 		a_token = Token.new(Token::INTEGER, 5)
-		b_token = Token.new(Token::INTEGER, 2)
 		node = Num.new(a_token)
 
 		it "Inherits from the AST base class" do	
@@ -17,6 +16,24 @@ describe AST do
 
 		it "Has a referance to the token used to create it" do
 			expect(node.token).to eql(a_token)
+		end
+	end
+
+	describe 'Binary Operator node' do
+		a_token = Token.new(Token::INTEGER, 2)
+		b_token = Token.new(Token::INTEGER, 5)
+		op_token = Token.new(Token::PLUS, '+')
+
+		leaf_left = Num.new(a_token)
+		leaf_right = Num.new(b_token)
+		root = BinOp.new(leaf_left, op_token, leaf_right)
+
+		it "Inherits from the AST base class" do
+			expect(root.is_a? AST).to eql(true)
+		end
+
+		it "Evaluates the operator node" do
+			expect(root.left.value + root.right.value).to eql(7)
 		end
 	end
 end
