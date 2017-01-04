b11fb09ef3299ed937f1ea4149a76c4085556b22
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-04 22:51:13 +0100

    Add test cases for parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,12 +4,39 @@ describe Parser do
 	describe '.parse' do
 		context 'Given a single value n' do
 			it 'Creates a single value 1' do
-				root = Parser.new( Lexer.new('2') ).parse
+				root = Parser.new( Lexer.new('1') ).parse
 				expect(root.value).to eql(1)
 			end
 			it 'Creates a single value 2' do
 				root = Parser.new( Lexer.new('2') ).parse
 				expect(root.value).to eql(2)
+			end
+		end
+
+		context 'Given a single operator' do
+			it 'Creates a tree of expression 2 + 3' do
+				root = Parser.new( Lexer.new('2 + 3') ).parse
+				expect(root.op.value).to eql('+')
+				expect(root.left.value).to eql(2)
+				expect(root.right.value).to eql(3)
+			end
+			it 'Creates a tree of expression 0 - 3' do
+				root = Parser.new( Lexer.new('0 - 3') ).parse
+				expect(root.op.value).to eql('-')
+				expect(root.left.value).to eql(0)
+				expect(root.right.value).to eql(3)
+			end
+			it 'Creates a tree of expression 3 / 1' do
+				root = Parser.new( Lexer.new('3 / 1') ).parse
+				expect(root.op.value).to eql('/')
+				expect(root.left.value).to eql(3)
+				expect(root.right.value).to eql(1)
+			end
+			it 'Creates a tree of expression 3 * 2' do
+				root = Parser.new( Lexer.new('3 * 2') ).parse
+				expect(root.op.value).to eql('*')
+				expect(root.left.value).to eql(3)
+				expect(root.right.value).to eql(2)
 			end
 		end
 	end
