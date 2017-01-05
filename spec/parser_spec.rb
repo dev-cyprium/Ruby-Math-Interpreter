543bb823a90318e5e98d2e70f20420d9ce3ba075
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-05 09:10:54 +0100

    Add more test cases for parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -39,5 +39,25 @@ describe Parser do
 				expect(root.right.value).to eql(2)
 			end
 		end
+
+		context 'Given a math expression with priority' do
+			it 'Creates a tree of expression 3 + 2 * 1' do
+				root = Parser.new( Lexer.new('3 + 2 * 1') ).parse
+				expect(root.op.value).to eql('+')
+				expect(root.left.value).to eql(3)
+				expect(root.right.op.value).to eql('*')
+				expect(root.right.left.value).to eql(2)
+				expect(root.right.right.value).to eql(1)
+			end
+
+			it 'Creates a tree of expression 3 * 1 + 2' do
+				root = Parser.new( Lexer.new('3 * 1 + 2') ).parse
+				expect(root.op.value).to eql('+')
+				expect(root.left.op.value).to eql('*')
+				expect(root.left.left.value).to eql(3)
+				expect(root.left.right.value).to eql(1)
+				expect(root.right.value).to eql(2)
+			end
+		end
 	end
 end
