324b34ceeffc2a1c1d146401df8cbbed7db530d3
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-05 11:47:53 +0100

    Add all parser specs

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -81,11 +81,28 @@ describe Parser do
 		end
 
 		context 'Given an error' do
+			error = "Unexpected token in syntax"
+			par_error = "Parentesies not closed"
 			it 'Throws an Error on expression 2 +' do
-				expect{Parser.new( Lexer.new('2 + ') ).parse }.to raise_error('Unexpected token EOF after PLU')
+				expect{Parser.new( Lexer.new('2 + ') ).parse }.to raise_error(error)
 			end
 			it "Throws an Error on expression + 2 3" do
-				expect{Parser.new(Lexer.new '+ 2 3').parse }.to raise_error('Unexpected token INT after INT')
+				expect{Parser.new(Lexer.new '+ 2 3').parse }.to raise_error(error)
+			end
+			it "Throws an Error on expression 2 3 4" do
+				expect{Parser.new(Lexer.new '2 3 4').parse }.to raise_error(error)
+			end
+			it "Throws an Error on expression 2 ( 3" do
+				expect{Parser.new(Lexer.new '2 ( 3').parse }.to raise_error(par_error)
+			end
+			it "Throws an Error on expression 2 * ( 3 + 2 )) + 2" do
+				expect{Parser.new(Lexer.new '2 ( 3').parse }.to raise_error(par_error)
+			end
+			it "Throws an Error on expression 2 * ( 3 + 2 * ( 3 + 2 )" do
+				expect{Parser.new(Lexer.new '2 * ( 3 + 2 * ( 3 + 2 )').parse }.to raise_error(par_error)
+			end
+			it "Throws an Error on expression 2 ()" do
+				expect{Parser.new(Lexer.new '2 ()').parse }.to raise_error(error)
 			end
 		end
 	end
