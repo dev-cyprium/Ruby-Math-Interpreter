8f6781ae2fd9e79c7053a5d2eda2a495fd02907b
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-05 20:17:34 +0100

    Add reverse polish notation tests

--- spec/interpreter_spec.rb
+++ spec/interpreter_spec.rb
@@ -12,5 +12,9 @@ describe Interpreter do
 			par = Parser.new( Lexer.new( '2 + 3 - 1 * 2' ) )
 			expect(Interpreter.new(par).to_reverse_polish).to eql("2 3 + 1 2 * -")
 		end
+		it "Converts 2 + 3 / ( 3 - 1) * (3 + ( 4 / ( 3 * 1 * 2) / 2) - 1) to reverse polish notation" do
+			par = Parser.new( Lexer.new('2 + 3 / ( 3 - 1) * (3 + ( 4 / ( 3 * 1 * 2) / 2) - 1)') )
+			expect(Interpreter.new(par).to_reverse_polish).to eql("2 3 3 1 - / 3 4 3 1 * 2 * / 2 / + 1 - * +")
+		end
 	end
 end
