0629a4c075a0733b0fccb9e7d2995ddccdc42ce3
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-05 20:22:17 +0100

    Add missing specs

--- spec/interpreter_spec.rb
+++ spec/interpreter_spec.rb
@@ -8,11 +8,15 @@ describe Interpreter do
 	end
 
 	context 'Given an expression' do
+		it "Calculates 2 + 3 - 1 * 2" do
+			par = Parser.new( Lexer.new( '2 + 3 - 1 * 2' ) )
+			expect(Interpreter.new(par).interpret).to eql(3)
+		end
 		it "Converts 2 + 3 - 1 * 2 to reverse polish notation" do
 			par = Parser.new( Lexer.new( '2 + 3 - 1 * 2' ) )
 			expect(Interpreter.new(par).to_reverse_polish).to eql("2 3 + 1 2 * -")
 		end
-		it "Converts 2 + 3 / ( 3 - 1) * (3 + ( 4 / ( 3 * 1 * 2) / 2) - 1) to reverse polish notation" do
+		it "Converts 2+3/(3-1)*(3+(4/(3*1*2)/2)-1) to reverse polish notation" do
 			par = Parser.new( Lexer.new('2 + 3 / ( 3 - 1) * (3 + ( 4 / ( 3 * 1 * 2) / 2) - 1)') )
 			expect(Interpreter.new(par).to_reverse_polish).to eql("2 3 3 1 - / 3 4 3 1 * 2 * / 2 / + 1 - * +")
 		end
