50d81173170fadb845a1d015177d806e7785d3c3
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-05 11:50:17 +0100

    Add constructor for interpreter

--- lib/interpreter.rb
+++ lib/interpreter.rb
@@ -1,6 +1,9 @@
 #
-# Class used to interpret tokens
-#
+# Class used to interpret AST
+# feed in the AST ( Abstract syntax tree you want to calculate )
 class Interpreter
 	
+	def initialize(ast)
+		@root = ast
+	end
 end
